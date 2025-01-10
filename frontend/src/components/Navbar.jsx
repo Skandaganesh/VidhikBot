cf903e29d235edf298d57e046e7c2b57c8a410fa
Skanda Ganesh P V <skandaganeshpv@gmail.com>
2025-01-11 01:33:01 +0530

    midway  integration

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = ({ closeNavbarAndChatBot }) => {
   return (
-    <nav className="sticky z-10 bg-blue-500 bg-opacity-30 backdrop-filter backdrop-blur-lg fixed top-0 left-0 w-full">
+    <nav className="sticky z-10 top-0 bg-gradient-to-r from-gray-700 via-gray-800 to-black text-white shadow-lg">
       <div className="flex items-center justify-between px-6 py-4">
         <h1
-          className="text-lg font-bold text-yellow-400 cursor-pointer"
+          className="text-lg font-bold text-yellow-400 cursor-pointer metamorphous-regular"
           onClick={closeNavbarAndChatBot}
         >
           VidhikBot
